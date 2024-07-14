efd147a969a2dfe59e431e7f8327a10d3b5edfae
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2024-07-14 18:46:01 +0200

    Finished, Re-opening not possible nicely

--- src/main/java/net/justonedev/mc/warpbooks/WarpPage.java
+++ src/main/java/net/justonedev/mc/warpbooks/WarpPage.java
@@ -28,6 +28,7 @@ import java.util.UUID;
 
 public class WarpPage implements Listener {
 
+	public static final String itemLore = "§7Empty Warp page";
 	public static final String itemName = "§fWarp page";
 	public static ItemStack warpPage;
 	
@@ -36,6 +37,7 @@ public class WarpPage implements Listener {
 		ItemMeta meta = warpPage.getItemMeta();
 		assert meta != null;
 		meta.setDisplayName(itemName);
+		meta.setLore(Collections.singletonList(itemLore));
 		meta.setCustomModelData(8499);
 		warpPage.setItemMeta(meta);
 	}
@@ -123,23 +125,13 @@ public class WarpPage implements Listener {
 	private static final HashMap<UUID, Location> signs = new HashMap<>();
 	
 	private Sign getNewSign(UUID uuid, Location loc) {
-		Location l = new Location(loc.getWorld(), loc.getX(), Objects.requireNonNull(loc.getWorld()).getMaxHeight() - 1, loc.getZ());
-		l = loc.clone();
+		Location l = loc.clone();
 		if (l.getBlock().getType() != Material.AIR) {
 			l.add(0, 1, 0);
 			if (l.getBlock().getType() != Material.AIR) {
 				return null;
 			}
 		}
-		
-		/*int i = 0;
-		while (l.getBlock().getType() != Material.AIR) {
-			l.add(1, 0, 0);
-			if (i++ > 60) {
-				break;
-			}
-		}*/
-		//if (l.getBlock().getType() != Material.AIR) previousBlocks.put(uuid, new Triple<>(l.getBlock().getType(), l.getBlock().getBlockData().clone(), l.getBlock().));
 		l.getBlock().setType(Material.BIRCH_WALL_SIGN);
 		signs.put(uuid, l.getBlock().getLocation());	// Round to block location
 		return (Sign) l.getBlock().getState();
@@ -156,22 +148,6 @@ public class WarpPage implements Listener {
 		
 		e.getBlock().setType(Material.AIR);
 		
-		/*
-		Location l = e.getBlock().getLocation();
-		
-		Bukkit.broadcastMessage("§e" + previousBlocks.containsKey(uuid));
-		
-		// Remove the sign block
-		if (previousBlocks.containsKey(uuid)) {
-			Bukkit.broadcastMessage("§b" + previousBlocks.get(uuid));
-			Triple<Material, BlockData> prev = previousBlocks.get(uuid);
-			previousBlocks.remove(uuid);
-			l.getBlock().setType(prev.Key);
-			l.getBlock().setBlockData(prev.Value);
-		} else {
-			l.getBlock().setType(Material.AIR);
-		}*/
-		
 		ItemStack page = p.getInventory().getItemInMainHand();
 		if (!isWarpPage(page)) return;
 		ItemMeta meta = page.getItemMeta();
@@ -180,8 +156,10 @@ public class WarpPage implements Listener {
 		String line = e.getLine(0);
 		if (line == null || line.isEmpty()) {
 			// Give old name back
-			if (!meta.hasLore() || Objects.requireNonNull(meta.getLore()).isEmpty()) return;
-			meta.setDisplayName(meta.getLore().get(0));
+			if (meta.hasLore() && !Objects.requireNonNull(meta.getLore()).isEmpty()) {
+				meta.setDisplayName(meta.getLore().get(0));
+			}
+			meta.setLore(Collections.singletonList(itemLore));
 			page.setItemMeta(meta);
 			return;
 		}
@@ -195,6 +173,7 @@ public class WarpPage implements Listener {
 		meta.getPersistentDataContainer().set(WarpBooks.pageZKey, PersistentDataType.DOUBLE, l.getZ());
 		meta.getPersistentDataContainer().set(WarpBooks.pagePitchKey, PersistentDataType.FLOAT, l.getPitch());
 		meta.getPersistentDataContainer().set(WarpBooks.pageYawKey, PersistentDataType.FLOAT, l.getYaw());
+		meta.setLore(Collections.emptyList());
 		page.setItemMeta(meta);
 	}
 	
@@ -213,35 +192,35 @@ public class WarpPage implements Listener {
 		e.blockList().removeIf((b) -> signs.containsValue(b.getLocation()));
 	}
 	
+	private static final double MAX_VELOCITY = 3, MAX_DISTANCE = 7.5;
+	
 	@EventHandler
 	public void onSignDistanceChanged(PlayerMoveEvent e) {
 		if (!signs.containsKey(e.getPlayer().getUniqueId())) return;
-		Bukkit.broadcastMessage("§aDistance: " + e.getPlayer().getLocation().distance(signs.get(e.getPlayer().getUniqueId())));
-		
-		
 		Location loc = signs.get(e.getPlayer().getUniqueId());
+		
+		//Bukkit.broadcastMessage("§eVelocity: " + e.getPlayer().getVelocity().length());
+		
 		if (loc == null) return;
 		try {
 			Sign old = ((Sign) loc.getBlock().getState());
-			if (old.getAllowedEditor() == null) {
+			if (old.getAllowedEditor() == null || e.getPlayer().getVelocity().length() >= MAX_VELOCITY || e.getPlayer().getLocation().distance(signs.get(e.getPlayer().getUniqueId())) > MAX_DISTANCE) {
+				// Remove old and return
+				e.getPlayer().sendMessage("§cPlease don't move too far away when setting warp pages.");
 				loc.getBlock().setType(Material.AIR);
-				
-				Sign sign = getNewSign(e.getPlayer().getUniqueId(), e.getPlayer().getLocation());
-				Bukkit.broadcastMessage("§dNew Sign: " + sign);
-				if (sign == null) {
-					// Remove old and return
-					signs.remove(e.getPlayer().getUniqueId());
-					return;
-				}
-				String s = old.getSide(Side.FRONT).getLine(0);
-				sign.getSide(Side.FRONT).setLine(0, s);
-				sign.update();
-				e.getPlayer().openSign(sign);
-			}
+				signs.remove(e.getPlayer().getUniqueId());
+			} else return;
 		} catch (Exception any) {
 			loc.getBlock().setType(Material.AIR);
 			signs.remove(e.getPlayer().getUniqueId());
 		}
+		
+		ItemStack page = e.getPlayer().getInventory().getItemInMainHand();
+		if (!isWarpPage(page)) return;
+		ItemMeta meta = page.getItemMeta();
+		if (meta == null) return;
+		meta.setLore(Collections.singletonList(itemLore));
+		page.setItemMeta(meta);
 	}
 	
 }
